3ec3f9dcf94094cbfe307354b284f06dbf12d5d2
Jared Beller <jbellerb@vt.edu>
2023-04-09 21:33:07 -0400

    document `DmxBuffer` and more `From`s

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,6 +1,32 @@
 use std::fmt::{self, Display, Formatter};
 use std::ops::{Deref, Index, IndexMut};
 
+/// A fixed-size byte buffer representing the state of a single DMX universe.
+///
+/// `DmxBuffer` holds exactly 512 bytes and is stored on the heap. All OLA
+/// methods dealing with retrieving or setting universes are done with
+/// `DmxBuffer`s. Various "buffer-like" types including `[u8; 512]` and
+/// `Vec<u8>` can be converted to and from `DmxBuffer`s, but must be exactly
+/// 512 bytes long for conversion to succeed.
+///
+/// # Examples
+///
+/// ```
+/// # use ola::DmxBuffer;
+/// let mut universe = DmxBuffer::new();
+/// assert_eq!(*universe, [0; 512]);
+/// assert_eq!(universe, [0; 512].into());
+///
+/// universe[0] = 255;
+/// assert_eq!(universe[0], 255);
+///
+/// let mut other = universe.clone();
+/// assert_eq!(other[0], 255);
+///
+/// other.zero();
+/// assert_eq!(*other, [0; 512]);
+/// assert_ne!(*universe, [0; 512]);
+/// ```
 #[derive(Clone, Debug, Eq)]
 pub struct DmxBuffer(Box<[u8; 512]>);
 
@@ -17,16 +43,66 @@ impl PartialEq for DmxBuffer {
 }
 
 impl From<[u8; 512]> for DmxBuffer {
-    fn from(value: [u8; 512]) -> Self {
-        Self(Box::new(value))
+    /// Construct a DMX buffer and move `b`'s values into it.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::DmxBuffer;
+    /// assert_eq!(DmxBuffer::from([0; 512]), DmxBuffer::new());
+    /// ```
+    fn from(b: [u8; 512]) -> Self {
+        Self(Box::new(b))
+    }
+}
+
+impl From<&[u8; 512]> for DmxBuffer {
+    /// Construct a DMX buffer and copy `b`'s values into it.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::DmxBuffer;
+    /// assert_eq!(DmxBuffer::from(&[0; 512]), DmxBuffer::new());
+    /// ```
+    fn from(b: &[u8; 512]) -> Self {
+        Self(Box::new(*b))
+    }
+}
+
+impl From<DmxBuffer> for Vec<u8> {
+    /// Convert a DMX buffer into a vector by transferring ownership of the
+    /// existing heap allocation.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::DmxBuffer;
+    /// assert_eq!(Vec::<u8>::from(DmxBuffer::new()), vec![0; 512]);
+    /// ```
+    fn from(b: DmxBuffer) -> Self {
+        (b.0 as Box<[u8]>).into()
     }
 }
 
 impl TryFrom<&[u8]> for DmxBuffer {
     type Error = TryFromBufferError;
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        let buffer = value.try_into().map_err(|_| TryFromBufferError(()))?;
+    /// Tries to create a DMX buffer by copying from a slice. Succeeds if
+    /// `b.len() == 512`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::{DmxBuffer, TryFromBufferError};
+    /// # fn main() -> Result<(), TryFromBufferError> {
+    /// let bytes = [0; 1024];
+    ///
+    /// assert_eq!(DmxBuffer::try_from(&bytes[0..512])?, DmxBuffer::new());
+    /// # Ok(()) }
+    /// ```
+    fn try_from(b: &[u8]) -> Result<Self, Self::Error> {
+        let buffer = b.try_into().map_err(|_| TryFromBufferError(()))?;
 
         Ok(Self(Box::new(buffer)))
     }
@@ -35,10 +111,23 @@ impl TryFrom<&[u8]> for DmxBuffer {
 impl TryFrom<Vec<u8>> for DmxBuffer {
     type Error = TryFromBufferError;
 
-    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
-        let buffer = value.try_into().map_err(|_| TryFromBufferError(()))?;
+    /// Construct a DMX buffer by transferring ownership of a vector's existing
+    /// heap allocation. Succeeds if `b.len() == 512`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::{DmxBuffer, TryFromBufferError};
+    /// # fn main() -> Result<(), TryFromBufferError> {
+    /// let bytes = vec![0; 512];
+    ///
+    /// assert_eq!(DmxBuffer::try_from(bytes)?, DmxBuffer::new());
+    /// # Ok(()) }
+    /// ```
+    fn try_from(b: Vec<u8>) -> Result<Self, Self::Error> {
+        let buffer = b.try_into().map_err(|_| TryFromBufferError(()))?;
 
-        Ok(Self(Box::new(buffer)))
+        Ok(Self(buffer))
     }
 }
 
@@ -51,10 +140,35 @@ impl Deref for DmxBuffer {
 }
 
 impl DmxBuffer {
+    /// Construct an empty (zeroed) DMX buffer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::DmxBuffer;
+    /// let universe = DmxBuffer::new();
+    ///
+    /// assert_eq!(*universe, [0; 512]);
+    /// ```
     pub fn new() -> Self {
         Default::default()
     }
 
+    /// Set all values in a DMX buffer to zero.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use ola::DmxBuffer;
+    /// let mut universe = DmxBuffer::new();
+    ///
+    /// universe[0] = 255;
+    /// universe[1] = 128;
+    /// assert_ne!(*universe, [0; 512]);
+    ///
+    /// universe.zero();
+    /// assert_eq!(*universe, [0; 512]);
+    /// ```
     pub fn zero(&mut self) {
         self.0.fill(0);
     }
@@ -74,12 +188,12 @@ impl IndexMut<usize> for DmxBuffer {
     }
 }
 
-/// The error type returned when a conversion to a dmx buffer fails.
+/// The error type returned when a conversion to a DMX buffer fails.
 #[derive(Clone, Debug)]
 pub struct TryFromBufferError(());
 
 impl Display for TryFromBufferError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "failed to convert to dmx buffer")
+        write!(f, "failed to convert to DMX buffer")
     }
 }
